1581bc350d8c2d91027b0d3de5ddf1c71b1b91e9
Konstantin Lapine <konstantin.lapine@forgerock.com>
2021-08-06 12:40:16 -0700

    chore: update debugger code and comments

--- src/scripted-debugger.js
+++ src/scripted-debugger.js
@@ -5,7 +5,7 @@
  * in ForgeRock Access Management (AM).
  *
  * @author Konstantin Lapine <Konstantin.Lapine@forgerock.com>
- * @version 0.3.4
+ * @version 0.3.5
  * @license MIT
  */
 
@@ -21,8 +21,8 @@
     logs.push('transientState: ' + transientState);
 
     /**
-     * You can save logs as a JSON stringified array
-     * in the sharedState object up the authentication journey,
+     * You can save log messages as a JSON stringified array
+     * in the `sharedState` object up the authentication journey,
      * and consume them in the debugger node.
      */
     var savedLogs = sharedState.get('logs');
@@ -44,31 +44,27 @@
 // An example of sending log messages to the debugger function: END
 
 /**
- * Display scripted decision log messages in a named pop-up window and/or in the login screen.
+ * Display scripted decision log messages (logs) in a named pop-up window and/or in the login screen.
+ * @see [ScriptTextOutputCallback]{@link https://backstage.forgerock.com/docs/am/7.1/authentication-guide/authn-supported-callbacks.html#backchannel-callbacks} (ForgeRock documentation).
+ * @see [TextOutputCallback]{@link https://backstage.forgerock.com/docs/am/7.1/authentication-guide/authn-supported-callbacks.html#read-only-callbacks} (ForgeRock documentation).
  *
- * @see [ScriptTextOutputCallback]{@link https://backstage.forgerock.com/docs/am/7.1/authentication-guide/authn-supported-callbacks.html#backchannel-callbacks}.
- * @see [TextOutputCallback]{@link https://backstage.forgerock.com/docs/am/7.1/authentication-guide/authn-supported-callbacks.html#read-only-callbacks}.
- *
- * You can change the pop-up CSS by adding style definitions to the style array.
- * You can change the log messages presentation in other ways by changing the script array items.
- * You can change the initial pop-up window dimensions by modifying the debuggerWindowWidthExpression and debuggerWindowHeightExpression variables.
- * @see getPopupScript
- *
- * @param {object} options
- * @param {array} options.logs An array of logs.
- * @param {boolean} [options.noPopup=undefined] Do NOT show logs in a pop-up window.
- * @param {boolean} [options.noText=undefined] Do NOT show current log in the login screen.
+ * You can change the pop-up CSS and the initial pop-up window dimensions
+ * by modifying arguments for [getPopupScript(options)]{@link #showLogs..getPopupScript} (inner method) in the debugger code.
+ * @param {object} [options={}]
+ * @param {string[]|string} [options.logs="No log information has been provided."] An array of logs or a single string representing a log.
+ * @param {boolean} [options.noPopup] Do NOT show logs in the pop-up window.
+ * @param {boolean} [options.noText] Do NOT show current log in the login screen.
  * CAUTION: the callbacks form will auto-submit if no login form callbacks are detected!
- * Make sure your journey has another stopping point (that is, a node with a callback) to avoid loops on failed login.
+ * In that case, make sure your journey has another stopping point (that is, a node with a callback) to avoid loops on failed login.
  * @param {string} [options.popupTitle=Debugger] Window title for the pop-up window.
- * @param {boolean} [options.noLoggerError=undefined] Do NOT output the logs with the logger.error(String message) method.
- * If not provided or falsy, each log message will be outputted with the logger object method.
- * @param {boolean} [options.useDebugParameter=undefined]
- * Require "debug" parameter in the URL query string for displaying the log messages in the browser.
- * For example, &debug=true.
- * If there is no "debug" parameter, or no value or a falsy value is provided (for example, &debug=false), the debugger will not be used.
- * This will not affect the use of  the logger.error(String message) method.
- *
+ * @param {boolean} [options.noLoggerError] Do NOT output the logs with the `logger.error(String message)` method.
+ * If not provided or falsy, each log will be outputted with the logger object method.
+ * @param {boolean} [options.useDebugParameter]
+ * Require `debug` parameter in the authentication journey URL query string for displaying the logs in the browser.
+ * If `useDebugParameter` is `true`, then in order for the debugger to be used
+ * the `debug` parameter needs to be present in the URL,
+ * and its value needs to be a truthy one; for example, `&debug=true`.
+ * Applying `useDebugParameter` will not affect the use of the `logger.error(String message)` method.
  * @returns {undefined} The function sends callbacks to the client side, but otherwise, returns nothing.
  * After the callbacks are completed, the function will exit the script setting its outcome to "true".
  * Thus, "true" is the expected outcome to be added to the scripted decision node that is using this code.
@@ -78,35 +74,49 @@ function showLogs (options) {
     var getTextContent;
     var getTextContentModifierScript;
     var processLog;
+    var logs;
+    var noPopup;
+    var noText;
+    var popupTitle;
+    var noLoggerError;
+    var useDebugParameter;
+
+    options = options || {};
+
+    logs = options.logs || 'No log information has been provided.';
+    noPopup = options.noPopup;
+    noText = options.noText;
+    popupTitle = options.popupTitle || 'Debugger';
+    noLoggerError = options.noLoggerError;
+    useDebugParameter = options.useDebugParameter;
 
     /**
-     * Ensure options.logs is an array.
+     * Ensure `logs` is an array.
      */
-    if (!Array.isArray(options.logs)) {
-        options.logs = [options.logs];
+    if (!Array.isArray(logs)) {
+        logs = [logs];
     }
 
     var frJava = JavaImporter(
         org.forgerock.openam.auth.node.api.Action
     );
 
-    if (!options.noLoggerError) {
-        options.logs.forEach(function (log) {
+    if (!noLoggerError) {
+        logs.forEach(function (log) {
             logger.error(String(log));
         });
     }
 
-    var debug = callbacks.isEmpty() && options.logs.length && !(options.noPopup && options.noText);
+    var debug = callbacks.isEmpty() && logs.length && !(noPopup && noText);
 
-    if (options.useDebugParameter) {
+    if (useDebugParameter) {
         var debugParameter = requestParameters.get('debug');
 
         debug = debug && debugParameter && ['false', 'null', 'undefined'].indexOf(String(debugParameter.toArray()[0])) === -1;
     }
 
     /**
-     * Send callbacks to the client on the first visit,
-     * and if there is something to show.
+     * Send callbacks to the client on the first visit and if there is something to show.
      */
     if (debug) {
         /**
@@ -183,31 +193,46 @@ function showLogs (options) {
 
         /**
          * Define a script to run on the client side.
-         *
-         * @returns {string}
+         * @param {object} [options]
+         * @param {string|number} [options.debuggerWindowWidthExpression=(screen.availWidth - innerWidth)]
+         * Define the initial pop-up window width with JavaScript code to run on the client side or a hardcoded value.
+         * By default, it will try to size it to fill the remaining space on the screen on left from the (parent) login window.
+         * @param {string|number} [options.debuggerWindowHeightExpression=screen.availHeight]
+         * Define the initial pop-up window height with JavaScript code to run on the client side or a hardcoded value.
+         * By default, it will try to use all available screen height.
+         * @param {string[]} [options.css=[ "* { font-family: 'Open Sans', sans-serif }", "p.date {color: green}", "div.log {margin-bottom: 8px;}" ]]
+         * Internal CSS to be applied in the debugger window.
+         * @returns {string} The script.
          */
-        getPopupScript = function () {
+        getPopupScript = function (options) {
             var script = [];
 
+            options = options || {};
+            options.debuggerWindowWidthExpression = options.debuggerWindowWidthExpression || '(screen.availWidth - innerWidth)';
+            options.debuggerWindowHeightExpression = options.debuggerWindowHeightExpression || 'screen.availHeight';
+            if (!options.css) {
+                options.css = [];
+                options.css.push('* { font-family: "Open Sans", sans-serif }');
+                options.css.push('p.date {color: green}');
+                options.css.push('div.log {margin-bottom: 8px;}');
+            }
+
             /**
-             * Define the initial pop-up window dimensions with a JavaScript code to run on the client side.
-             *
-             * By default, it will be sized to fill the remaining space on the screen
-             * left from the (parent) login window.
-             *
-             * You can use hardcoded values instead.
+             * Create a pop-up window.
              */
-            var debuggerWindowWidthExpression = '(screen.availWidth - innerWidth)';
-            var debuggerWindowHeightExpression = 'screen.availHeight';
-            var popupTitle = options.popupTitle || 'Debugger';
+            script.push('var p = open(\'\', \'debuggerWindow\', \'scrollbars=yes, width=\' + ' + options.debuggerWindowWidthExpression + ' + \', height=\' + ' + options.debuggerWindowHeightExpression + ');');
 
-            script.push('var p = open(\'\', \'debuggerWindow\', \'scrollbars=yes, width=\' + ' + debuggerWindowWidthExpression + ' + \', height=\' + ' + debuggerWindowHeightExpression + ');');
+            /**
+             * Add CSS to the pop-up window.
+             */
+            script.push('var style = p.document.createElement(\'style\');');
 
+            /**
+             * Write logs.
+             */
             script.push('p.document.write(\'<p class="date">\' + Date() + \'</p>\');');
-
             script.push('p.document.title = "' + popupTitle + '";');
-
-            options.logs.forEach(function (log) {
+            logs.forEach(function (log) {
                 var processedLog = String(processLog({
                     scripted: true,
                     log: log,
@@ -219,27 +244,16 @@ function showLogs (options) {
 
                 script.push('p.document.write(\'' + processedLog + '\');');
             });
-
             script.push('p.document.body.scrollTop = p.document.body.scrollHeight;');
-
-            /**
-             * The CSS to be applied in the pop-up window.
-             */
-            var style = [];
-            style.push('* { font-family: "Open Sans", sans-serif }');
-            style.push('p.date {color: green}');
-            style.push('div.log {margin-bottom: 8px;}');
-
-            script.push('var style = document.createElement(\'style\');');
             script.push('style.type = "text/css";');
-            script.push('style.appendChild(document.createTextNode(\'' + style.join(' ') + '\'));');
+            script.push('style.appendChild(p.document.createTextNode(\'' + options.css.join(' ') + '\'));');
             script.push('p.document.head.appendChild(style);');
 
             /**
              * Auto-submit the callbacks form if no text is to be displayed in the login screen.
              * CAUTION: might create a loop on login failure IF there are no login form callbacks detected!
              */
-            if (options.noText) {
+            if (noText) {
                 script.push('if (!document.querySelector(\'div[role="alert"], div[role="presentation"]\')) { \n\
                     document.querySelector(\'button[type="submit"]\').click(); \n\
                 }');
@@ -250,13 +264,12 @@ function showLogs (options) {
 
         /**
          * Define text content to be displayed in the login screen.
-         *
          * @returns {string}
          */
         getTextContent = function () {
             var content = [];
 
-            options.logs.forEach(function (log) {
+            logs.forEach(function (log) {
                 content.push(String(processLog({
                     log: log,
                     container: {
@@ -270,8 +283,7 @@ function showLogs (options) {
         };
 
         /**
-         * Define a script to allow HTML to be displayed in the login screen.
-         *
+         * Define a script to allow log content HTML to be displayed in the login screen.
          * @returns {string}
          */
         getTextContentModifierScript = function () {
@@ -295,12 +307,12 @@ function showLogs (options) {
         );
 
         var callbacksToSend = [];
-        if (!options.noPopup) {
+        if (!noPopup) {
             callbacksToSend.push(frJavaCallbacks.ScriptTextOutputCallback(
                 getPopupScript()
             ));
         }
-        if (!options.noText) {
+        if (!noText) {
             callbacksToSend.push(frJavaCallbacks.TextOutputCallback(
                 frJavaCallbacks.TextOutputCallback.ERROR,
                 getTextContent()
